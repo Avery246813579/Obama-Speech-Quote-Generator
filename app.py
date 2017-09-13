932916d9bf0cc0212b4307a894d48e730f43068c
Avery Durrant <avery246813579@gmail.com>
2017-09-13 10:09:39 -0700

    Tried to fix port issue

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 import Main
+import os
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 @app.route('/')
 def hello_world():
