393c30c18b2b35a2b98145fce81c239b2a186dd1
Avery Durrant <avery246813579@gmail.com>
2017-09-13 00:21:02 -0700

    Finished sampling

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import random
 import time
+
 # args = ["Test", "Dog", "cat"]
 #
 # to_print = ''
@@ -35,23 +36,65 @@ class FileParser:
 
 
 class Histogram:
-    data = {}
+    data = []
     raw = 0
 
     def __init__(self, words):
         for i in range(len(words)):
-            self.add(words[i], None)
+            self.add(words[i])
 
-    def add(self, key, value):
+        self.calculate_percents()
+
+    def calculate_percents(self):
+        last = 0
+
+        for i in range(len(self.data)):
+            if len(self.data[i]) > 2:
+                self.data[i][2] = last + self.data[i][0] * len(self.data[i][1]) / self.raw
+            else:
+                self.data[i].append(last + self.data[i][0] * len(self.data[i][1]) / self.raw)
+
+            last += self.data[i][0] / self.raw * len(self.data[i][1])
+
+    def add(self, key):
         self.raw += 1
+        length = len(self.data)
 
-        if key in self.data:
-            self.data[key] += 1
+        if length < 1:
+            self.data.append([1, [key]])
+            return
+
+        for i in range(length):
+            if key in self.data[i][1]:
+                self.data[i][1].remove(key)
+
+                if i > length - 2:
+                    self.data.append([self.data[i][0] + 1, [key]])
+                else:
+                    if self.data[i + 1][0] == self.data[i][0] + 1:
+                        self.data[i + 1][1].append(key)
+                    else:
+                        self.data.insert(i + 1, [self.data[i][0] + 1, [key]])
+
+                if len(self.data[i][1]) < 1:
+                    del self.data[i]
+
+                return
+
+        if self.data[0][0] == 1:
+            self.data[0][1].append(key)
         else:
-            self.data[key] = 1
+            self.data.insert(0, [key])
 
     def length(self):
         return len(self.data)
+
+    def random_word(self):
+        number = random.random()
+
+        for i in range(len(self.data)):
+            if number < self.data[i][2]:
+                return self.data[i][1][random.randint(0, len(self.data[i][1]) - 1)]
 
     def frequency(self, key):
         if key in self.data:
@@ -62,4 +105,5 @@ class Histogram:
 
 file = FileParser("test_data.txt")
 gram = Histogram(file.words)
-print(gram.frequency("fish"))
+print(gram)
+print(gram.random_word())
