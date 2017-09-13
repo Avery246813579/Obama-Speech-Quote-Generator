9da0e7c845351d7f90605d32546b40f4a80087c3
Avery Durrant <avery246813579@gmail.com>
2017-09-13 10:11:52 -0700

    Please fix the port issue

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 import Main
 import os
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!<h1>Dogs</h1>' + Main.gram.random_word()
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!<h1>Dogs</h1>' + Main.gram.random_word()
