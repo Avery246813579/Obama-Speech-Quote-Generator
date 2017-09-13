84b9c9778b90180d4fec21afa76d47c7821e072b
Avery Durrant <avery246813579@gmail.com>
2017-09-13 10:15:40 -0700

    Pretty please work :D

--- app.py
+++ app.py
@@ -9,6 +9,5 @@ def hello_world():
     return 'Hello, World!<h1>Dogs</h1>' + Main.gram.random_word()
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
