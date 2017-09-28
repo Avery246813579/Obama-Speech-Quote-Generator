bf7e28d2b3b841dcc2fb118ab05966c446eaf895
Avery Durrant <avery246813579@gmail.com>
2017-09-28 10:46:36 -0700

    Added things ot init.py and setup.py

---
+++ lib/__init__.py
@@ -0,0 +1,4 @@
+from MarkovModel import MarkovModel
+from Histogram import Histogram
+from FileParser import FileParser
+from Dictogram import Dictogram
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MarkovTweetGenerator',
+      version='0.0.1',
+      description='A Quote Generator using Markov Chains',
+      url='https://github.com/Avery246813579/Obama-Speech-Quote-Generator',
+      author='Avery Durrant',
+      author_email='avery@frostbyte.co',
+      license='MIT',
+      packages=['lib'],
+      zip_safe=False)
