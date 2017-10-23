d864f616589788fff373e5da09e8c35a80f753cb
Avery Durrant <avery246813579@gmail.com>
2017-10-23 12:52:21 -0700

    Removed the if main

--- app.py
+++ app.py
@@ -137,6 +137,5 @@ def default_route():
     return send_from_directory('react-website/build', 'index.html')
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
