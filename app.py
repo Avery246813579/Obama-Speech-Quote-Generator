679fed0ad8f67aca60f53c80ba3733ea54282b43
Avery Durrant <avery246813579@gmail.com>
2017-10-23 14:25:23 -0700

    Again wokring with Heroku v2

--- app.py
+++ app.py
@@ -122,12 +122,14 @@ def default_route():
 
 print("HERE3")
 
+print("ASDASD")
+model = RainbowChain('static/data/raw_corpus.txt', 3)
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
     print("HERE5")
 
-print("ASDASD")
-model = RainbowChain('static/data/raw_corpus.txt', 3)
 
 
