2fbd0f408ee0ea30f4938838bb4a033e523cc2e4
Avery Durrant <avery246813579@gmail.com>
2017-10-23 14:20:41 -0700

    Moved the MAIN

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ conn = psycopg2.connect("dbname=dds7q3a5dl5c45 user=edksigbbpxnyrh password=" +
 
 tweets = []
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    print("HERE5")
+
 print("ONE")
 model = RainbowChain('static/data/raw_corpus.txt', 3)
 print("TWO")
@@ -122,9 +127,6 @@ def default_route():
 
 print("HERE3")
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-    print("HERE5")
+
 
 print("HERE4")
