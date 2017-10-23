7bd32362816a37a168569d2c455961219deeddf4
Avery Durrant <avery246813579@gmail.com>
2017-10-23 12:28:58 -0700

    Added seeding

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -18,7 +18,7 @@ class Favorites extends Component {
         return (
             <div>
                 {this.props.favorites.map(function (item) {
-                    return <p key={item.toString()}>- {item}</p>
+                    return <p key={item.toString()}>"{item}"<br/><br/></p>
                 })}
             </div>
         )
@@ -34,21 +34,65 @@ class App extends Component {
         this.tweet = this.tweet.bind(this);
         this.componentWillMount = this.componentWillMount.bind(this);
         this.favorite = this.favorite.bind(this);
+    }
 
+    topicChanged() {
+        let topic = document.getElementById('topic');
+        let custom = document.getElementById('custom');
+
+        if (topic.selectedIndex === 4) {
+            custom.style.display = "inline";
+        } else {
+            if (custom.style.display !== "none") {
+                custom.style.display = "none";
+            }
+        }
     }
 
     refreshSentence() {
         let self = this;
+        let topic = document.getElementById('topic');
+        let custom = document.getElementById('custom');
+        let body = {};
+
+        if (typeof topic !== "undefined" && topic !== null) {
+            switch (topic.selectedIndex) {
+                case 1:
+                    body['seed'] = "America";
+                    break;
+                case 2:
+                    body['seed'] = "China";
+                    break;
+                case 3:
+                    body['seed'] = "United States";
+                    break;
+                case 4:
+                    if (custom.value !== "") {
+                        if (custom.value.split(" ").length > 3) {
+                            self.setState({quote: "Seed can't be more then 3 words"});
+                            return;
+                        }
+
+                        body['seed'] = custom.value;
+                    }
+
+                    break;
+            }
+        }
 
         fetch(self.state.url + '/new', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json'
             },
-            body: JSON.stringify({test: ""})
+            body: JSON.stringify(body)
         }).then((response) => response.json())
             .then(function (responseJson) {
-                self.setState({quote: responseJson['data'], id: responseJson['id']});
+                if (responseJson['success']) {
+                    self.setState({quote: responseJson['data'], id: responseJson['id']});
+                } else {
+                    self.setState({quote: "Could not find sentence with seed", id: -1});
+                }
             })
             .catch((error) => {
                 console.log(error);
@@ -124,10 +168,6 @@ class App extends Component {
                 <img src={logo} style={{maxWidth: "100%"}} alt="logo"/>
 
                 <div id="quote">
-                    <div className="header">
-                        Yes, we did. Yes, we can.
-                    </div>
-
                     <div className="text">
                         <div id="sentence">
                             <Quote text={this.state.quote}/>
@@ -144,44 +184,51 @@ class App extends Component {
                     <div className="buttons" style={{color: "white"}}>
                         Topic:&nbsp;
 
-                        <select>
+                        <select id="topic" onChange={this.topicChanged}>
                             <option value="0">
                                 Random
                             </option>
                             <option value="1">
                                 America
                             </option>
-                            <option>
+                            <option value="2">
                                 China
                             </option>
-                            <option>
+                            <option value="3">
+                                United States
+                            </option>
+                            <option value="4">
                                 Custom
                             </option>
                         </select>
+
+                        <input id="custom" style={{display: "none", marginLeft: "10px"}} type="text"
+                               placeholder="Topic"/>
                     </div>
                 </div>
 
-                <div id="about">
+                <div className="about">
                     <div className="header">
-                        AN IN-DEPTH LOOK AT THIS PROJECT
+                        Some of our favorite tweets
                     </div>
                     <br/>
-                    <center><a href="https://github.com/Avery246813579/Obama-Speech-Quote-Generator">This project</a>
-                        was made by <a
-                            href="https://www.linkedin.com/in/avery-durrant-676402148/">Avery Durrant</a> using Markov
-                        Chains.
-                    </center>
-                    <center>The Corpus I used was has a lot lines of text and a few words. It was taken from
-                        over 200 of
-                        Barack Obama's Speeches.
-                    </center>
 
-                    <div>
-                        Favorites:
-                    </div>
                     <div id="favorites">
                         <Favorites favorites={this.state.favorites}/>
                     </div>
+                </div>
+
+                <div className="about" style={{top: "-140px"}}>
+                    <div className="header">
+                        About this project
+                    </div>
+                    <br />
+                    <a href="https://github.com/Avery246813579/Obama-Speech-Quote-Generator">This project</a>&nbsp;
+                    was made by
+                    <a href="https://www.linkedin.com/in/avery-durrant-676402148/">Avery Durrant</a>&nbsp;
+                    using Markov Chains. The Corpus I used was has a lot lines of text and a few words. It was taken from
+                    over 200 of Barack Obama's Speeches. To view our tweets look at our
+                    &nbsp;<a href="https://twitter.com/WhatDidObamaSay">twitter account</a>
                 </div>
             </div>
         );
